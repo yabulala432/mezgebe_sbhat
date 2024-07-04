898b82fede2e80071d1a0080e0d3b8104584577d
Yeabsira Yonas <yabulala432@gmail.com>
2024-07-04 23:01:12 +0300

    add theme for the project

---
+++ lib/themes/dark.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData dark = ThemeData(
+  colorScheme: const ColorScheme.dark(
+    background: Color(0xff212832),
+    primary: Color(0xffee631e),
+    secondary: Color(0xffffffff),
+    inversePrimary: Color(0xff212832),
+    error: Color(0xffee631e),
+  ),
+);
---
+++ lib/themes/light.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+  colorScheme: const ColorScheme.light(
+    background: Color.fromARGB(255, 176, 113, 63),
+    primary: Color.fromARGB(255, 63, 23, 3),
+    secondary: Color(0xffffffff),
+    inversePrimary: Color.fromARGB(255, 176, 113, 63),
+    error: Color.fromARGB(255, 238, 30, 30),
+  ),
+);
