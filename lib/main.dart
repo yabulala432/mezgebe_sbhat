b9261491368a55d0b9766b0eab143b07181b91c3
Yeabsira Yonas <yabulala432@gmail.com>
2024-08-14 12:50:49 +0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mezgebe_sbhat/providers/theme_provider.dart';
 import 'package:mezgebe_sbhat/screens/drawer_navigator.dart';
 import 'package:mezgebe_sbhat/services/file_service.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
